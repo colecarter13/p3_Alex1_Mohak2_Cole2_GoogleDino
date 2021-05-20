ea20f4de212ad5c2e47c0a2aced4ffeb3704eb22
colecarter <colecarter@coles-air.lan>
2021-05-20 12:35:19 -0700

    making base code for project

--- DinoProject/src/MainPanel.java
+++ DinoProject/src/MainPanel.java
@@ -1,90 +1,107 @@
 import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class MainPanel implements MouseListener  {
-	//change the size of the board if you need to
-	int gridRows =  8, gridCols = 8;
-	Tile[][] tiles = new Tile[gridRows][gridCols];
+public class MainPanel extends JPanel implements ActionListener, KeyListener{
+	//handles drawing animation
+	Timer animationTimer; 
+	
+	
+	public void paint(Graphics g) {
+		//calling this line ensures the frame is redrawn
+		super.paintComponent(g);
+		
+		//call paint methods of objects or through g.drawRect etc
+	
+		
+	}
 	
 	/* constructor for MainPain class */
 	public MainPanel() {
 		
 		//Create a JFrame Object with a title bar text
-		JFrame f = new JFrame("Dino Game");
+		JFrame f = new JFrame("Example Drawing");
 		
 		//Set the size of the window
-		f.setSize(800,800); //width and height
+		f.setSize(800,600); //width and height
 		
 		//set default action for x button
 		//without this, your code will run behind the scenes until
 		//you force exit
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setLayout(new GridLayout(gridRows,gridCols));
 		
-		
-		//setup the board
-		for(int r = 0; r < tiles.length; r++) {
-			for(int c = 0; c < tiles[r].length; c++) {
-				tiles[r][c] = new Tile("queen.png", r, c);				
-				
-				//add the title to the jFrame
-				f.add(tiles[r][c]);
-				
-				//add mouse listener for each tile
-				tiles[r][c].addMouseListener(this);
-			}
-		}
+		//add this panel to the JFrame
+		//allows connection with "drawing"
+		f.add(this);
+		f.addKeyListener(this);
 		
 		
 		
+		//setup animation timer
+		animationTimer = new Timer(16, this);
 		
+		//do not forget to start the timer
+		animationTimer.start();
+		
+		//instantiate the rest of the instance variables
 		
 		
 		
 		f.setVisible(true);
 	}
 
+	/* this method is invoked/called by the titmer */
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent arg0) {
 		
-		//On a click, figure out what tile was clicked
-		Tile theTile = (Tile) arg0.getComponent();
-		System.out.println(theTile.r);
+		//call the frame to refresh
+		//recall that actitonPerformed is called by the
+		//timer object every 16ms
+		repaint();
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getKeyCode());
+		
+		
+		switch(arg0.getKeyCode()) {
+		//if keycode is 'd' key
+		case 68:
+			
+			break;
+			
+		case 65:
+			System.out.println("stuff for left key using a");
+			break;
+			
+		}
+		
 		
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
+	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseExited(MouseEvent arg0) {
+	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	
 	
 }
