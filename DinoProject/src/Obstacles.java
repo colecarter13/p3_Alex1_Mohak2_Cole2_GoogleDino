d4a29017d999d62ee13ad5b190afa44f9350de99
colecarter <colecarter@coles-air.lan>
2021-06-01 13:21:55 -0700

    Worked on Obstacle class

--- DinoProject/src/Obstacles.java
+++ DinoProject/src/Obstacles.java
@@ -1,10 +1,44 @@
+
+import java.awt.Color;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 public class Obstacles {
- private int x, y;
+ private int x = 0, y = 0;
  private int vx, vy;
  private Image img;
  private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
  private int acc;
+ private int coin;
+ 
+ public Obstacles() {
+	 coin = (int) (Math.random()* 2) + 1;
+	 System.out.println(coin);
+	 if(coin > 1) {
+		 img = getImage("unnamed.png"); 
+	 }
+	 
+	 init(x, y);
+ }
+ 
+ 
+ 
+    private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(1, 1);
+	}
+	
+	
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Dino.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
 }
